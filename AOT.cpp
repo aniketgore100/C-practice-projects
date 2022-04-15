7562f83af83007407689315368451343140d9509
aniketgore100 <77752124+aniketgore100@users.noreply.github.com>
2022-04-15 11:25:18 +0530

    Add files via upload

---
+++ AOT.cpp
@@ -0,0 +1,22 @@
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+void solve(){
+    int x;
+    cin>>x;
+    cout<<x-1<<" "<<x<<" "<<x+1<<endl;
+    return;
+}
+
+
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--){
+        solve();
+    }
+    return 0;
+}
