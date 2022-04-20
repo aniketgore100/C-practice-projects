e59af7252a3eeda5ddcebaa676a5a12ada997f03
aniketgore100 <77752124+aniketgore100@users.noreply.github.com>
2022-04-20 20:25:14 +0530

    Add files via upload

---
+++ bitwise.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int a, b;
+    cin>>a>>b;
+    cout<<"and"<<(a&b)<<endl;
+    cout<<"or"<<(a|b)<<endl;
+    cout<<"not"<<(~a)<<endl;
+    cout<<"xor"<<(a^b)<<endl;
+}
