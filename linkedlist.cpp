46a49ad0ce053e09afbcc56b1d1bdf321e305bdf
Aniket <goreaniket100@gmail.com>
2023-05-13 00:35:39 +0530

    changes accured

--- linkedlist.cpp
+++ linkedlist.cpp
@@ -1,27 +1,84 @@
-#include <iostream>
-
+#include <bits/stdc++.h>
 using namespace std;
 
-struct ListNode {
-    int val;
-    ListNode *next;
-    ListNode() : val(0), next(nullptr) {}
-    ListNode(int x) : val(x), next(nullptr) {}
-    ListNode(int x, ListNode *next) : val(x), next(next) {}
+class node{
+public:
+int data;
+node* next;
+
+// node(int  data){
+//     this->data = data;
+//     this->next = NULL;
+// }
+
 };
 
 
-int main() {
-    ListNode *head = new ListNode(1);
-    head->next = new ListNode(2);
-    head->next->next = new ListNode(3);
+void insertFront(node* &head, int data) {
+    node* newnode = new node();
+    newnode->data = data;
+    newnode->next = head;
+    head = newnode;
+}
 
-    ListNode *curr = head;
-    while (curr != nullptr) {
-        cout << curr->val << " ";
-        curr = curr->next;
+
+void insertAtEnd(node* &head, int data) {
+    node* newnode = new node();
+    newnode->data = data;
+    newnode->next = NULL;
+    if(head == NULL){
+        head = newnode;
     }
-    cout << endl;
+    else{
+      node* temp = head;
+      while(temp->next != NULL){
+        temp = temp->next;
+      }
+      temp->next = newnode;
+    }
+   
+}
 
+
+void ShowList(node* n){
+    while(n != NULL){
+        cout<<n->data<<"->";
+        n = n->next;
+    }
+    cout<<endl;
+}
+
+int main(){
+    node* head = NULL;
+    node* current = NULL;
+    int n, value;
+    cin>>n;
+    head = new node();
+    cin>>value;
+    head->data = value;
+    head->next = NULL;
+
+    current = head;
+    for(int i = 2; i<=n; i++){
+        node* newnode = new node();
+        cin>>value;
+        newnode->data = value;
+        newnode->next = NULL;
+
+        current->next = newnode;
+        current = newnode;
+    }
+
+
+    ShowList(head);
+    cout<<"Insert a new node at Front "<<endl;
+    cin>>value;
+    insertFront(head, value);
+    ShowList(head);
+
+    cout<<"Insert a new node at end "<<endl;
+    cin>>value;
+    insertAtEnd(head, value);
+    ShowList(head);
     return 0;
 }
